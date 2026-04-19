da8495ee8bad0e65d93a891e372870f5dc3395a6
myk <165669834+mykcs@users.noreply.github.com>
2026-04-19 21:06:31 +0800

    chore(sprites-gallery): upgrade astro to 6.1.8

---
+++ public/sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'sprites-gallery-v1';
+const STATIC_ASSETS = [
+  '/sprites-gallery/',
+  '/sprites-gallery/favicons/icon-192x192.png',
+  '/sprites-gallery/favicons/icon-32x32.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(STATIC_ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request).then((response) => {
+        if (!response || response.status !== 200) return response;
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+        return response;
+      });
+    })
+  );
+});
